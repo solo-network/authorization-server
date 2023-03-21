0a2d9d8e33dedd71a64e374a3d23c45b3828cd26
Felipe <felipegatefy@gmail.com>
2023-03-21 12:45:08 +0000

    update tamplate

--- django_oauth_server/settings.py
+++ django_oauth_server/settings.py
@@ -76,12 +76,6 @@ WSGI_APPLICATION = 'django_oauth_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 if os.getenv("Djangoenv") == 'True':
     DATABASES = {
